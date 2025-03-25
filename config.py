cc1acff12310f0ed96755c2cf060d69f7ac6f16d
Ignatius32 <ignaciobasti@gmail.com>
2025-03-25 20:59:02 -0300

    corrected config for ok database uri name 3

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     GAS_DEPLOYMENT_URL = os.environ.get('GAS_DEPLOYMENT_URL')
     ADMIN_NAME = os.environ.get('ADMIN_NAME')
